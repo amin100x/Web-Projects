0b940e4f5b403660640444a1b1bd5627ab39a270
Mahmadamin <lodhiyaamin88@gmail.com>
2022-01-14 12:08:53 +0530

    all projects

---
+++ Change button color/cbn.js
@@ -0,0 +1,91 @@
+
+var allbutton = document.getElementsByTagName('button');
+console.log(allbutton);
+
+var copyallbutton = [];
+for (let i = 0; i < allbutton.length; i++) {
+    copyallbutton.push(allbutton[i].classList[1]);
+}
+console.log(copyallbutton);
+
+function buttonColorChange(buttoncolor) {
+    if (buttoncolor.value === 'Red') {
+        buttonred();
+    }
+    else if (buttoncolor.value === 'Green') {
+        buttongreen();
+    }
+    else if(buttoncolor.value === 'Yellow')
+    {
+        buttonyellow();
+    }
+    else if(buttoncolor.value === 'Blue')
+    {
+        buttonblue();
+    }
+    else if(buttoncolor.value==='Reset')
+    {
+        buttonreset();
+    }
+    else if(buttoncolor.value==='Random')
+    {
+        buttonrandom();
+    }
+
+}
+
+function buttonred()
+{
+    for(let i=0; i<allbutton.length ; i++)
+    {
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add('btn-danger');
+    }
+}
+
+function buttongreen()
+{
+    for(let i=0; i<allbutton.length ; i++)
+    {
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add('btn-success');
+    }
+}
+function buttonyellow()
+{
+    for(let i=0; i<allbutton.length ; i++)
+    {
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add('btn-warning');
+    }
+}
+function buttonblue()
+{
+    for(let i=0; i<allbutton.length ; i++)
+    {
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add('btn-primary');
+    }
+}
+
+function buttonreset()
+{
+    for(let i=0; i<allbutton.length; i++)
+    {
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add(copyallbutton[i]);
+    }
+}
+
+function buttonrandom()
+{
+    var color=['btn-primary', 'btn-danger', 'btn-success', 'btn-warning'];
+   
+    for(let i=0; i<allbutton.length ; i++)
+    {
+        var randomnum= Math.floor(Math.random()*4);
+        allbutton[i].classList.remove(allbutton[i].classList[1]);
+        allbutton[i].classList.add(color[randomnum]);
+    }
+
+}
